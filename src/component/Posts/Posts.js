cdefcb9ce0adbbdad548fbe7399a1b691d5c0268
PavelBerikov <NoooB199505@gmail.com>
2023-10-08 13:51:53 +0300

    lesson4hw

--- src/component/Posts/Posts.js
+++ src/component/Posts/Posts.js
@@ -5,15 +5,16 @@ import {postsService} from "../services/posts.service";
 const Posts = () => {
     const {state} = useLocation();
     const [post, setPost] = useState({});
+    const {userId, id, title, body} = post
     useEffect(() => {
         postsService.getById(state.postId).then(value => value.data).then(value => setPost({...value}))
         console.log('Done!')
     }, [state.postId])
     return (
         <div>
-            <div>User Id - {post.userId}</div>
-            <div>Title - {post.title}</div>
-            <div>{post.body}</div>
+            <div>User Id - {userId}</div>
+            <div>Title - {title}</div>
+            <div>{body}</div>
         </div>
     );
 };
