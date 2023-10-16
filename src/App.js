39006335935c44421b14b968860780e66d583305
PavelBerikov <NoooB199505@gmail.com>
2023-10-16 12:14:15 +0300

    lesson 5

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from 'react';
 import {Posts} from "./components/Posts";
 import {Comments} from "./components/Comments";
 import {Cars} from "./components/Cars";
+import {Link, Route, Routes} from "react-router-dom";
+import {Home} from "./components/home/Home";
+import './index.css';
 
 const App = () => {
     return (
         <div>
-            <Cars/>
-            {/*<Comments/>*/}
-            {/*<Posts/>*/}
+            <Link to={'/'}><button className={'home'}>Home</button></Link>
+            <div className={'container'}>
+                <Link to={'posts'}><button className={'menu'}>Posts</button></Link>
+                <Link to={'comments'}><button className={'menu'}>Comments</button></Link>
+                <Link to={'cars'}><button className={'menu'}>Cars</button></Link>
+            </div>
+            <Link to={'cat&dog'}><button className={'catdog'}>Cats and Dogs</button></Link>
+            <Routes>
+                <Route path={'/'} element={<Home/>}></Route>
+                <Route path={'posts'} element={<Posts/>}></Route>
+                <Route path={'comments'} element={<Comments/>}></Route>
+                <Route path={'cars'} element={<Cars/>}></Route>
+            </Routes>
         </div>
     );
 };
 
 export default App;
+
+/*використовуючи хук редусер в якому початковий стейт уде масив котів/собак
+* реалізувати збереження окремо нового кота та нового собаку приклад на зображенні
+* реалізувати видалення для кожного пи натисканні на кнопку делейт*/
