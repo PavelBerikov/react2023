58739917d8e577a736abdf961724c98304b5da0b
PavelBerikov <NoooB199505@gmail.com>
2024-01-31 14:29:08 +0200

    base redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 const App = () => {
+    const store = useSelector(state => state);
+    const dispatch = useDispatch();
+    const increment = () => {
+        dispatch({type: 'INC', payload: 2})
+    };
+    const decrement = () => {
+        dispatch({type: 'DEC', payload: 3})
+    };
+    const reset = () => {
+        dispatch({type: 'RES'})
+    };
     return (
         <div>
-            app
+            <h2>{store}</h2>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={reset}>reset</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 
-
+const baseReducer = (state = 0, action) => {
+    switch (action.type){
+        case 'INC':
+            return state + action.payload
+        case 'DEC':
+            return state - action.payload
+        case 'RES':
+            return 0
+        default:
+            return state
+    };
+};
+const store = createStore(baseReducer);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <Provider store={store}>
         <App/>
-    </BrowserRouter>
+    </Provider>
 
 );
 
