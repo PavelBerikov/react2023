0b86edcfd7fab85bc0d24f72d41cbb98b0e972a4
PavelBerikov <NoooB199505@gmail.com>
2024-02-15 18:15:00 +0200

    lesson 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {MainLayout} from "./layouts/MainLayout";
+import {CarsPage} from "./pages/CarsPage";
 
 const App = () => {
     return (
-        <div>
-            app
-        </div>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'cars'}/>}/>
+                <Route path={'cars'} element={<CarsPage/>}/>
+            </Route>
+        </Routes>
     );
 };
 
-export default App;
+export {App};
